af9f34a031a10a295e14d95e5aa09f87c44fd9c8
SENG Sourng <sourng2009@gmail.com>
2019-03-13 22:14:13 +0700

    Dashboard
    Change Login Dashboard

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.backend.master')
+
+@section('pagetitle','Add New Student Form')
+
+@push('css')
+	{{-- expr --}}
+@endpush
+
+@section('main-content')
+
+<h1>Dashboard</h1>
+
+@endsection
+
+@push('css')
+	{{-- expr --}}
+@endpush
